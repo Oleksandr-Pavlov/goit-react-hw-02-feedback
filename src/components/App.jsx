4aa94d96d078f8b68fabca3f3c14339a279e6609
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-11-29 19:21:10 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,6 @@ import { FeedbackForm } from "./Feedback/FeedbackForm";
 
 export const App = () => {
   return (
-    <FeedbackForm
-      good={0}
-      neutral={0}
-      bad={0}
-      total={0}
-      positivePercentage={0} />
+    <FeedbackForm />
   );
 };
