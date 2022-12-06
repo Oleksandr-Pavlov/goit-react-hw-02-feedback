939e5fac7434809824eb0c4708f29ac56761f59d
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-12-06 23:21:23 +0200

    upd

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.js
@@ -4,30 +4,16 @@ import css from '../FeedbackOptions/FeedbackOptions.module.css';
 export const FeedbackOptions = ({ options, onLeaveFeedback } ) => {
   return (
     <div className={css.buttonWrapper}>
-      <button
-        type="button"
-        name={options[0]}
-        onClick={onLeaveFeedback}
-        className={css.button}
-      >
-        Good
-      </button>
-      <button
-        type="button"
-        name={options[1]}
-        onClick={onLeaveFeedback}
-        className={css.button}
-      >
-        Neutral
-      </button>
-      <button
-        type="button"
-        name={options[2]}
-        onClick={onLeaveFeedback}
-        className={css.button}
-      >
-        Bad
-      </button>
+      {options.map(option => (
+        <button
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+          className={css.button}
+        >
+          {option}
+        </button>
+      ))}
     </div>
   );
 };
